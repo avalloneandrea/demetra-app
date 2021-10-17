daa98fe7d172a0522c248b2f244a14fb917ec179
Andrea Avallone <avalloneandrea@gmail.com>
2021-10-17 09:55:24 +0200

    Add header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import packageInfo from '../../package.json';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
-  title = 'demetra-app';
+
+  appVersion: string = packageInfo.version;
+
 }
