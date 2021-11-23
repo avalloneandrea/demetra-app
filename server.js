7832b32e066a88296c11c92d892d8536074c2615
Andrea Avallone <avalloneandrea@gmail.com>
2021-11-23 14:03:45 +0100

    Add internazionalization

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
-app.get('/*', (req, res) => res.sendFile(__dirname + '/dist/index.html'));
+app.get('/*', (req, res) => res.sendFile(__dirname + '/dist/it/index.html'));
 app.listen(process.env.PORT || 4200);
--- src/app/pipes/recipe-ingredients/recipe-ingredient.pipe.ts
+++ src/app/pipes/recipe-ingredients/recipe-ingredient.pipe.ts
@@ -9,21 +9,21 @@ export class RecipeIngredientPipe implements PipeTransform {
 
   transform(value: RecipeIngredient): string {
 
-    const quantity = value?.quantity || '';
-    const unit = value?.ingredient?.unit || '';
     const name = value?.ingredient?.name || '';
+    const unit = value?.ingredient?.unit || '';
+    const quantity = value?.quantity || '';
 
     if (unit === 'g')
-      return `${ quantity } g di ${ name.toLowerCase() }`;
+      return `${ quantity } ${ unit } ${ $localize `of` } ${ name.toLowerCase() }`;
 
     if (unit === 'u')
       return `${ quantity } ${ name.toLowerCase() }`;
 
     if (unit === 'd')
-      return `${ quantity } ${ quantity === 1 ? 'dose' : 'dosi' } di ${ name.toLowerCase() }`;
+      return `${ quantity } ${ quantity === 1 ? $localize `dose` : $localize `doses` } ${ $localize `of` } ${name.toLowerCase()}`;
 
     if (unit === 'qb')
-      return `${ name } q.b.`;
+      return `${ name } ${ unit }`;
 
     return '';
 
