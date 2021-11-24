86e42d294ce0f376d33a2d30b975a6c7c29b431c
Andrea Avallone <avalloneandrea@gmail.com>
2021-11-24 12:05:04 +0100

    Improve internazionalization

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
-app.get('/*', (req, res) => res.sendFile(__dirname + '/dist/it/index.html'));
+app.get('/:locale([a-z]{2})/*', (req, res) => res.sendFile(__dirname + '/dist/' + req.params.locale + '/index.html'));
+app.get('/*', (req, res) => res.redirect('/it'));
 app.listen(process.env.PORT || 4200);
